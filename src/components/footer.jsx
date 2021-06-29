d50eaf6ede121384429fbb6468a1c82606a57a65
MarianaLimon <marianalimong@hotmail.com>
2021-06-28 21:53:28 -0500

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./footer.css"
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="footer"></div>
+      </footer>
+    );
+  }
+}
+
+
+export default Footer;
---
+++ src/services.js
@@ -0,0 +1,7 @@
+// URL Base
+const API_URL = "http://localhost:8080";
+
+// Endpoints
+const postURL = `${API_URL}/posts/`;
+const postDetailURL = (id) => `${API_URL}/posts/${id}/`;
+const postLoginURL = `${API_URL}/users/`
