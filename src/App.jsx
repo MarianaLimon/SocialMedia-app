38066d8a329550b4c3c98ef8b332bd9f509dc0b4
Mary <oxesescom@gmail.com>
2021-06-30 17:08:19 -0500

    created class avatar and avatar-profile

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,27 @@ import Icons from "./components/commons/icons";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import CheckboxScreen from "./screens/CheckboxScreen";
+import Test from "./screens/Test.js";
 
 function App() {
   return (
     <React.Fragment>
-      <Icons value={'doctor'}/>
-      <Icons value={'home'}/>
+      <Icons value={"doctor"} />
+      <Icons value={"home"} />
 
       <Router>
-        <Route exact path="/checkbox">
+        <Switch>
+          <Route exact path="/checkbox">
             <CheckboxScreen />
-        </Route>        
+          </Route>
+          <Route exact path="/test" component={Test} />
+        </Switch>
       </Router>
 
       <div className="App">
         <p>Holiiiii</p>
         <Footer />
       </div>
-      
     </React.Fragment>
   );
 }
