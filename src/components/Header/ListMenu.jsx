a1a8f95b27269ca8910a9420b41b9f6c7c00ee20
Mary <oxesescom@gmail.com>
2021-07-01 12:38:35 -0500

    create menu hamburger

---
+++ src/components/Header/ListMenu.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+import Styles from "./listMenu.module.css";
+
+export default function ListMenu({ open }) {
+  const [closeMenu, setCloseMenu] = useState("d-none");
+  const [openMenu, setOpenMenu] = useState("d-none");
+
+  useEffect(() => {
+    if (open) {
+      setOpenMenu("");
+    }
+  }, [open]);
+  const close = () => {
+    setOpenMenu("d-none");
+    open = !open;
+  };
+
+  return (
+    <div className={`${openMenu} ${Styles.MenuMobile}  d-block d-md-none`}>
+      <div className={`${Styles.MenuWrapper}`}>
+        <div className={`${Styles.MenuTitle}`}>
+          <h2>Menu</h2>
+
+          <button className={`${Styles.MenuCloseBtn}`} type="button">
+            <span onClick={close}>x</span>
+          </button>
+        </div>
+
+        <ul>
+          <li className="nav-item active">
+            <a href="/">Inicio</a>
+          </li>
+          <li className="nav-item active">
+            <a href="/">Usuarios</a>
+          </li>
+          <li className="nav-item active">
+            <a href="/">Productos</a>
+          </li>
+          <li className="nav-item active">
+            <a href="/">Articulos</a>
+          </li>
+          <li className="nav-item active">
+            <a href="/">Webinars</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header/MenuHamburger.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+import ListMenu from "./ListMenu";
+import Styles from "./menuHamburger.module.css";
+import Icons from "../commons/icons";
+
+export default function MenuHamburger() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className={`${Styles.navbarToggler} d-block d-md-none`}
+        type="button"
+        open={open}
+        onClick={() => setOpen(!open)}
+      >
+        <Icons value="menu" />
+      </button>
+      <ListMenu open={open} />
+    </>
+  );
+}
