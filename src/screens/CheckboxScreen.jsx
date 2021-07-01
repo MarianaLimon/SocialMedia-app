9f1333ca75733d2de265ee2afff5c2e8d549fd41
jedmundo-rm <jedmundo.rm@gmail.com>
2021-07-01 17:43:46 -0500

    modal app

--- src/screens/CheckboxScreen.jsx
+++ src/screens/CheckboxScreen.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AppCheckboxColumns from "../components/commons/AppCheckboxColumns";
 import AppCheckboxRow from "../components/commons/AppCheckboxRow";
 
+import AppModal from "../components/commons/AppModal"
+
 //import Styles from "../../App.css";
 
 export default function Home() {
+
+  const [show, setShow] = useState(false);
+
   return (
     <React.Fragment>
 
             <AppCheckboxColumns />
 
             <AppCheckboxRow />
+
+
+            <button onClick={() => setShow(true)}>Show Modal</button>
+            <AppModal title="Modal Title" onClose={() => setShow(false)} show={show}>
+              <p>This is modal body</p>
+            </AppModal>
+
 
     </React.Fragment>
   );
