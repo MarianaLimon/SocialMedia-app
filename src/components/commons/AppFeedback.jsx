603d0bae2eb5e6dfc61289356ff395b5acc115f5
Beto Barrera <betomonter@gmail.com>
2021-07-08 19:38:40 -0500

    update component for feedback validation

--- src/components/commons/AppFeedback.jsx
+++ src/components/commons/AppFeedback.jsx
@@ -4,7 +4,11 @@ import "./appFeedback.css";
 function AppFeedback({ feedback, className, props }) {
   const erroMessage = feedback ? feedback : "";
 
-  return <small className={`text-danger ${className}`}>{erroMessage}</small>;
+  return (
+    <small className={`text-danger ${className ? className : ""}`}>
+      {erroMessage}
+    </small>
+  );
 }
 
 export default AppFeedback;
