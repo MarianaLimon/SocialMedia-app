5d4163cbab7eafb5ffcce95822d73166b0c0cadb
MarianaLimon <marianalimong@hotmail.com>
2021-07-09 14:33:40 -0500

    Create Component Draft & screen AgregarArticulo

--- src/screens/Admin/AddArticle/index.jsx
+++ src/screens/Admin/AddArticle/index.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import LeftMenu from "../../../components/LeftMenu";
-import { useHistory } from "react-router";
-import { getSpecialties } from "../../../services/specialties";
-import { postUser } from "../../../services/users";
 
 import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
 import Input from "../../../components/commons/AppInput";
 import AppButton from "../../../components/commons/AppButton";
 import AppSelect from "../../../components/commons/AppSelect";
-
-import AppFeedback from "../../../components/commons/AppFeedback";
-import AppDragDrop from "../../../components/commons/AppDragDrop";
+import EditorDraft from "../../../components/EditorDraft";
+import AppDragDrop from "../../../components/commons/AppDragDrop"
 
 export default function AddArticle() {
 
@@ -23,8 +19,8 @@ export default function AddArticle() {
           <div className="col-lg-2">
             <LeftMenu />
           </div>
-          <div className="col-lg-10">
-            <h1 className="mt-3 title-sections">
+          <div className="col-lg-10 px-lg-5">
+            <h1 className="mt-3 title-sections mb-4">
               <b>Agregar Artículo</b> 
             </h1>
             <form action="">
@@ -42,32 +38,24 @@ export default function AddArticle() {
                 value=""
                 required
               />
-              <Input
-                id="professional_license_url"
-                placeholder="Upload Foto Cédula"
-                type="hidden"
-                // value={professional_license_url}
-                // onChange={(event) =>
-                //   setProfessionalLicenseUrl(event.target.value)
-                // }
-                required
+              
+              <AppDragDrop
+                // stateUrl=""
+                // callbackSetState=""
+                textDragDrop="Arrastre la imagen principal"
+                textBrowse="seleccione el archivo"
               />
               
-
+              <EditorDraft />
 
               <AppSelect
                 classSelect="AppInput_InputComponent"
-                classLabel="col-12"
-                idSelect="especialidad"
-                placeholder="Especialidad"
+                classLabel="col-12 mb-3"
+                idSelect=""
+                placeholder=""
                 classContainerInput=""
-                // options={options}
-                // keyNameOption={1}
-                // keyNameValue={0}
-                // value={specialty_id}
-                // onChange={(event) => setSpecialtyId(event.target.value)}
               />
-              <AppButton classButton="aqua newArticle mt-1 mt-lg-3 mb-1"  type="submit" text="GUARDAR" />
+              <AppButton classButton="aqua newArticle d-block mx-auto"  type="submit" text="GUARDAR" />
             </form>
           </div>
         </div>     
