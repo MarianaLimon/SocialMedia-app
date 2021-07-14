d5987e99167d9811b548e16c2fed9f278b1500b8
Beto Barrera <betomonter@gmail.com>
2021-07-13 21:55:02 -0500

    login validation and chech button functionality

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,55 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 
-import banner from "../../img/doctora-banner.png"
+import banner from "../../img/doctora-banner.png";
 import Input from "../../components/commons/AppInput";
 import AppButton from "../../components/commons/AppButton";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import { postLogin } from "../../services/users"
+import { postLogin } from "../../services/users";
 import jwt_decode from "jwt-decode";
-
-
+import { isEmail } from "../../utils/validations";
+import AppFeedback from "../../components/commons/AppFeedback";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [home, setHome] = useState("")
+  //const [home, setHome] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const history = useHistory();
 
-  useEffect(() => {
+  //useEffect(() => {}, []);
 
-  }, [])
-
+  const handleValidation = () => {
+    let validForm = true;
+    if (!isEmail(email)) {
+      validForm = false;
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+    if (password.length < 6) {
+      validForm = false;
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+    return validForm;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    console.log("handleSubmit");
+    if (!handleValidation()) {
+      return false;
+    }
+    console.log("validation");
     try {
       const user = {
         email,
         password,
       };
-
-
+      console.log(user);
       const login = await postLogin(user);
 
       if (login.success) {
+        console.log("sucess", login.success);
         const { token } = login.data;
 
         const dataUser = jwt_decode(token);
 
         localStorage.setItem("token", token);
 
-        dataUser.role[0] === "medico" ? setHome("/home") : setHome("/homeadmin")
-        history.push(home)
+        dataUser.role[0] === "admin"
+          ? history.push("/homeadmin")
+          : history.push("/home");
       }
     } catch (error) {
       console.log(error);
     }
   };
-
 
   return (
     <React.Fragment>
@@ -69,13 +89,39 @@ export default function Login() {
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
               />
-              <Input placeholder="Contraseña" type="password" required
+              {emailError ? (
+                <AppFeedback
+                  className="moveup"
+                  feedback="Escriba un email valido"
+                />
+              ) : null}
+              <Input
+                placeholder="Contraseña"
+                type="password"
+                required
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
-
               />
-              <AppButton classButton="secondary w-50 d-block mx-auto mt-4" type="submit" text="INGRESAR" />
-              <p className="text-center">¿Aún no estás registrado?<AppButton classButton="aqua" type="anchor" text="Registrate aquí" url="/register" /></p>
+              {passwordError ? (
+                <AppFeedback
+                  className="moveup"
+                  feedback="Escriba un password de por lo menos 6 caracteres"
+                />
+              ) : null}
+              <AppButton
+                classButton="secondary w-50 d-block mx-auto mt-4"
+                type="submit"
+                text="INGRESAR"
+              />
+              <p className="text-center">
+                ¿Aún no estás registrado?
+                <AppButton
+                  classButton="aqua"
+                  type="anchor"
+                  text="Registrate aquí"
+                  url="/register"
+                />
+              </p>
             </form>
           </div>
         </div>
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,5 +1,5 @@
 export function isEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var regex = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
   }
 
