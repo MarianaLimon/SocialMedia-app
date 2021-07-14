2d8d1183f8088ffdc902b3a0c3223ea08bbf00db
Mary <oxesescom@gmail.com>
2021-07-14 09:18:57 -0500

    create function formatDate

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,24 @@
 import jwt_decode from "jwt-decode";
-export const whois=()=>{
+
+const { format } = require('date-fns')
+const { es } = require('date-fns/locale')
+
+
+export const whois = () => {
     if (localStorage.getItem("token")) {
         const userLogged = jwt_decode(localStorage.getItem("token"));
         return (userLogged);
-    }    
+    }
+}
+
+export const getDateFormat = (date) => {
+    return format(Date.parse(date), "dd 'de' MMMM yy p aaaa", {
+        locale: es
+    })
+}
+
+export const getDateFormatAdmin = (date) => {
+    return format(Date.parse(date), "P p aaaa", {
+        locale: es
+    })
 }
