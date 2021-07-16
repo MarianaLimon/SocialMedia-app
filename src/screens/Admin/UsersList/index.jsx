1142508c76532d9a680bc05b3bd653ea854a4c5a
Beto Barrera <betomonter@gmail.com>
2021-07-16 15:02:57 -0500

    reverse order for users and articles

--- src/screens/Admin/UsersList/index.jsx
+++ src/screens/Admin/UsersList/index.jsx
@@ -7,12 +7,11 @@ import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
 import { useHistory } from "react-router";
 
-import "./index.css"
-import react from "react";
+import "./index.css";
 
 export default function UsersList() {
   const [users, setUsers] = useState([]);
-  const history = useHistory()
+  const history = useHistory();
 
   useEffect(() => {
     const request = async () => {
@@ -31,29 +30,40 @@ export default function UsersList() {
       mother_lastname: motherLastName,
       email: email,
       status: status,
-      registerdate: registerDate
+      registerdate: registerDate,
     },
   ]) => {
     return (
       <React.Fragment>
         <tr key={idUser}>
-          <th scope="row"><AppCheckbox /></th>
+          <th scope="row">
+            <AppCheckbox />
+          </th>
           <td>{`${firstName} ${lastName}`}</td>
           <td className="d-lg-block d-none">{email}</td>
           <td>{status}</td>
-          <th scope="col"><button className={`btn-validate`} onClick={() => { history.push(`/user-validate/${idUser}`) }}><Icons value={'validate'} /></button></th>
+          <th scope="col">
+            <button
+              className={`btn-validate`}
+              onClick={() => {
+                history.push(`/user-validate/${idUser}`);
+              }}
+            >
+              <Icons value={"validate"} />
+            </button>
+          </th>
         </tr>
       </React.Fragment>
-
     );
   };
 
-
   const buildUsers = (users) => {
     if (Object.entries(users).length) {
-      return Object.entries(users).filter(user => user[1].role[0] == "medico").map(printUsers);
+      return Object.entries(users)
+        .filter((user) => user[1].role[0] == "medico")
+        .reverse()
+        .map(printUsers);
     }
-
   };
 
   return (
@@ -71,16 +81,22 @@ export default function UsersList() {
             <table className="table table-striped">
               <thead>
                 <tr key="users1">
-                  <th scope="col"><Icons value={'delete'} /></th>
-                  <th scope="col"><span className="table-show">Nombre</span></th>
-                  <th scope="col" className="d-lg-block d-none">Email</th>
-                  <th scope="col"><span className="table-show">Estatus</span></th>
+                  <th scope="col">
+                    <Icons value={"delete"} />
+                  </th>
+                  <th scope="col">
+                    <span className="table-show">Nombre</span>
+                  </th>
+                  <th scope="col" className="d-lg-block d-none">
+                    Email
+                  </th>
+                  <th scope="col">
+                    <span className="table-show">Estatus</span>
+                  </th>
                   <th scope="col"> </th>
                 </tr>
               </thead>
-              <tbody>
-                {buildUsers(users)}
-              </tbody>
+              <tbody>{buildUsers(users)}</tbody>
             </table>
           </div>
         </div>
